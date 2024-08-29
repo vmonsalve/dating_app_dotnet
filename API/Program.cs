9c6bdd2b0c3d56bc47fa759c1152aaf76350ad4d
Vicente Monsalve <vicente.monsalve@gmail.com>
2024-08-28 22:50:55 -0400

    finish section 3, create angular project

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
      opt.UseSqlServer(builder.Configuration.GetConnectionString("DevConection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .WithOrigins("http://localhost:3000", 
+                               "https://localhost:3000"
+                              )
+          );
 app.MapControllers();
 app.Run();
